0edd632ca41c0379723e0fd2db80c2d333ba2af9
lite3 <lite3@qq.com>
2013-11-11 17:46:32 +0800

    first version

---
+++ ClassName.py
@@ -0,0 +1,43 @@
+import sublime
+import sublime_plugin
+import os
+import os.path
+
+class ClassnameCopyCommand(sublime_plugin.WindowCommand):
+	def run(self, paths=None):
+		if paths:
+			file = '*'.join(paths)
+		else:
+			file = sublime.active_window().active_view().file_name()
+		
+		if file and len(file) > 0:
+			fullname = self.getClassName(file)
+			if fullname:
+				sublime.set_clipboard(str(fullname))
+				sublime.status_message("Copied class full name")
+			else:
+				sublime.status_message("Can not find class name")
+
+	def getClassName(self, file):
+		folders = sublime.active_window().folders()
+		(path, ext) = os.path.splitext(file)
+		extLen = len(ext)
+		for dir in folders:
+			if 0 == file.find(dir):
+				fullname = file[len(dir)+1:]
+				if extLen > 0:
+					fullname = fullname[0:-extLen]
+				fullname = fullname.replace('\\', '.')
+				return fullname
+		
+		return None
+
+
+
+	def is_visible(self, paths=None):
+		if paths:
+			file = '*'.join(paths)
+		else:
+			file = sublime.active_window().active_view().file_name()
+		return os.path.isfile(file)
+
